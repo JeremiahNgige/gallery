95ea1459e7a265ebe601a699658a91710c1874c2
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-10-12 19:48:16 +0300

    landing page

---
+++ gallery/static/js/index.js
@@ -0,0 +1,20 @@
+//top nav color change onScroll
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navTop").style.background = "white";
+    document.getElementById("navTop").style.boxShadow  = "0 8px 6px -6px rgba(0, 0, 0, 0.664)";
+    document.getElementById("navTop").style.webkitBoxShadow  = "0 8px 6px -6px rgba(0, 0, 0, 0.603)";
+    document.getElementById("navTop").style.mozBoxShadow  = "0 8px 6px -6px rgba(0, 0, 0, 0.678)";
+    document.getElementById("navTop").style.zIndex  = "99";
+    document.getElementById("navTop").style.transition ="background 0.5s"
+  } else {
+    document.getElementById("navTop").style.background = "transparent";
+    document.getElementById("navTop").style.boxShadow  = "0 8px 0px 0px rgba(0, 0, 0, 0.664)";
+    document.getElementById("navTop").style.webkitBoxShadow  = "0 0px 0px 0px rgba(0, 0, 0, 0.603)";
+    document.getElementById("navTop").style.mozBoxShadow  = "0 0px 0px 0px rgba(0, 0, 0, 0.678)";
+    
+  }
+}
--- pictures/settings.py
+++ pictures/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gallery'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'pictures.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER':'oem',
+        'PASSWORD':'Jeremih23'
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
