a2e699c04573f386e41cdc1e8992b3e060901813
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-10-13 13:13:50 +0300

    views home page

--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http  import HttpResponse,Http404
+from .models import Category, Image, location
+
 
 def home(request):
-    return render(request,'home.html')
+    '''
+    renders the landing page
+    '''
+    
+    images = Image.objects.all()
+    locations = location.get_locations()
+    return render(request,'home.html',{'images':images,'locations':locations})
