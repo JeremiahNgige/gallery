c7bc61b2aae3ce10447fce385678a8035072b5f8
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-10-13 22:29:14 +0300

    search by category

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     path('',views.home, name='home'),
--- pictures/settings.py
+++ pictures/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
+DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
